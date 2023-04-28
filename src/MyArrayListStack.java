a3139cb5369d6904c60db2758df78edbdd982470
Alikhan2215 <dochshanov_alikhan@mail.ru>
2023-04-28 23:47:38 +0600

    Declared an instance variable

--- src/MyArrayListStack.java
+++ src/MyArrayListStack.java
@@ -1,5 +1,7 @@
 import java.util.EmptyStackException;
 import java.util.ArrayList;
 public class MyArrayListStack {
+    //Declare an instance variable of type MyArrayList
+    private ArrayList myArrayList;
 
 }
