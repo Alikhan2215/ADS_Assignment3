e11d1afb35fc4d807aeea3773ef7d57166802c40
Alikhan2215 <dochshanov_alikhan@mail.ru>
2023-04-29 00:03:45 +0600

    Implement the pop method

--- src/MyArrayListStack.java
+++ src/MyArrayListStack.java
@@ -8,8 +8,18 @@ public class MyArrayListStack {
     public MyArrayListStack() {
         myArrayList = new ArrayList();
     }
+
     //5. Implement the push method that adds an element to the top of the stack
     public void push(Object element) {
         myArrayList.add(element); // Add the element to the end of the ArrayList
+    }
+
+    //6. Implement the pop method that removes and returns the top element of the stack
+    public Object pop() {
+        if (myArrayList.isEmpty()) {
+            throw new EmptyStackException(); // Throw an exception if the stack is empty
+        }
+        // Remove and return the last element of the ArrayList
+        return myArrayList.remove(myArrayList.size() - 1);
     }
 }
