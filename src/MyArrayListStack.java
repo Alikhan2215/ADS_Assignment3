e20f6d55a8fd12e25e98b1868d1acd9011941182
Alikhan2215 <dochshanov_alikhan@mail.ru>
2023-04-28 23:48:09 +0600

    Created constructor

--- src/MyArrayListStack.java
+++ src/MyArrayListStack.java
@@ -1,7 +1,12 @@
 import java.util.EmptyStackException;
 import java.util.ArrayList;
 public class MyArrayListStack {
-    //Declare an instance variable of type MyArrayList
+    //3. Declare an instance variable of type MyArrayList
     private ArrayList myArrayList;
+
+    //4. Create a constructor for the class and initialize the instance variable.
+    public MyArrayListStack() {
+        myArrayList = new ArrayList();
+    }
 
 }
