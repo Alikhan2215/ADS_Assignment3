eb109ea31a1d7b479cdc1ba25a5ff0d23802a9e5
Alikhan2215 <dochshanov_alikhan@mail.ru>
2023-04-29 22:55:24 +0600

    checked stack methods

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,17 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        MyArrayListStack stack = new MyArrayListStack();
+
+        System.out.println("Is stack empty? " + stack.isEmpty());
+
+        stack.push(1);
+        stack.push(2);
+        stack.push(3);
+
+        System.out.println("Stack size: " + stack.stackSize());
+        System.out.println("Stack top element: " + stack.peek());
+
+        System.out.println("Popped element: " + stack.pop());
+        System.out.println("Stack size after popping one element: " + stack.stackSize());
     }
 }
