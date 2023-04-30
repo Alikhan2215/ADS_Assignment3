a7c7dc0a7e9befb8efa0c9a4943a37324dab9834
Alikhan2215 <dochshanov_alikhan@mail.ru>
2023-04-30 10:56:50 +0600

    declared a constructor

--- src/MyLinkedListQueue.java
+++ src/MyLinkedListQueue.java
@@ -4,4 +4,10 @@ public class MyLinkedListQueue {
     //3. Declare an instance variable of type LinkedList
     private LinkedList myLinkedList;
 
+    //4. Create a constructor for the class and initialize the instance variable
+    public MyLinkedListQueue() {
+        myLinkedList = new LinkedList();
+    }
+
+    //5.
 }
