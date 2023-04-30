feeaa5c4f6664e73db7e9d7ad6cbd0e05ae97681
Alikhan2215 <dochshanov_alikhan@mail.ru>
2023-04-30 11:07:13 +0600

    Implement the dequeue method

--- src/MyLinkedListQueue.java
+++ src/MyLinkedListQueue.java
@@ -1,3 +1,4 @@
+import java.util.EmptyStackException;
 import java.util.NoSuchElementException;
 import java.util.LinkedList;
 public class MyLinkedListQueue {
@@ -12,5 +13,14 @@ public class MyLinkedListQueue {
     //5. Implement the enqueue method that adds an element to the back of the queue
     public void enqueue(Object element) {
         myLinkedList.addLast(element);
+    }
+
+    //6. Implement the dequeue method that removes and returns the front element of the queue
+    public Object dequeue() {
+        if (myLinkedList.isEmpty()) {
+            throw new EmptyStackException(); // Throw an exception if the stack is empty
+        }
+        // Remove and return the first element of the queue
+        return myLinkedList.removeFirst();
     }
 }
