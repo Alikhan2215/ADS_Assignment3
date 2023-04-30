d4305cb8221db745fbe45e07bf460d08750ea99f
Alikhan2215 <dochshanov_alikhan@mail.ru>
2023-04-30 10:56:27 +0600

    declared an instance

--- src/MyLinkedListQueue.java
+++ src/MyLinkedListQueue.java
@@ -1,5 +1,7 @@
 import java.util.NoSuchElementException;
 import java.util.LinkedList;
 public class MyLinkedListQueue {
+    //3. Declare an instance variable of type LinkedList
+    private LinkedList myLinkedList;
 
 }
